668d54d92f7edbb2bfb995609f0427fbb0693b1d
GabrielModin <gabriel.barzen@mailfence.com>
2021-12-22 16:19:37 +0100

    Added /stops as json

--- application/src/main/java/com/wikipathia/application/Application.java
+++ application/src/main/java/com/wikipathia/application/Application.java
@@ -1,5 +1,7 @@
 package com.wikipathia.application;
 
+import com.google.gson.Gson;
+import com.wikipathia.application.model.Stop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		Stop stop = new Stop();
+		stop.id = 1;
+		stop.name ="hej";
+		Gson gson = new Gson();
+		String json = gson.toJson(stop);
+		System.out.println(json);
 		SpringApplication.run(Application.class, args);
 	}
 
