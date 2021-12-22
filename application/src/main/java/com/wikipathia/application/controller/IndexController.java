bcdfb03b4460b5b766c7c3cc012172aca5b4b3e2
GabrielModin <gabriel.barzen@mailfence.com>
2021-12-22 14:27:06 +0100

    got html

--- application/src/main/java/com/wikipathia/application/controller/IndexController.java
+++ application/src/main/java/com/wikipathia/application/controller/IndexController.java
@@ -1,2 +1,24 @@
-package com.wikipathia.application.controller;public class IndexController {
+package com.wikipathia.application.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class IndexController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getIndex() {
+        return "TODO: INDEX PAGE"; //TODO: index-page
+    }
+
+    @RequestMapping(value = "/api/documentation", method = RequestMethod.GET)
+    public String getApiDocumentation() {
+        return "TODO: DOCUMENTATION PAGE"; //TODO: Doc-page
+    }
+    @RequestMapping(value = "/api/documentation/pages", method = RequestMethod.GET)
+    public String getApiDocumentationPages() {
+        return "TODO: DOCUMENTATION PAGE LIST"; //TODO: Doc-page
+    }
+    @RequestMapping(value = "/api/documentation/page/{id}", method = RequestMethod.GET)
+    public String getApiDocumentationPage(@PathVariable("id") int id) {
+        return "TODO: DOCUMENTATION PAGE" + id; //TODO: Doc-page
+    }
 }
