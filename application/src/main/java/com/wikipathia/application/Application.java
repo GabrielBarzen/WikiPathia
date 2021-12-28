15e321318d9dd582465a7f9cdc639ede9e75e433
GabrielModin <gabriel.barzen@mailfence.com>
2021-12-28 12:05:36 +0100

    get wiki article on lat/long

--- application/src/main/java/com/wikipathia/application/Application.java
+++ application/src/main/java/com/wikipathia/application/Application.java
@@ -1,21 +1,35 @@
 package com.wikipathia.application;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.wikipathia.application.controller.WikipediaService;
 import com.wikipathia.application.model.Stop;
+import com.wikipathia.application.model.WikipediaGson;
+import com.wikipathia.application.model.WikipediaPage;
+import com.wikipathia.application.model.WikipediaPages;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		Stop stop = new Stop();
-		stop.id = 1;
-		stop.name ="hej";
-		Gson gson = new Gson();
-		String json = gson.toJson(stop);
-		System.out.println(json);
-		SpringApplication.run(Application.class, args);
+		WikipediaService service = new WikipediaService(WebClient.builder());
+
+		List<WikipediaPage> list = service.getWikipediaPagesFromCoordinates(59.3293, 18.0686);
+		for (WikipediaPage page: list) {
+			System.out.println(page.pageid);
+		}
+		//SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ application/src/main/java/com/wikipathia/application/model/WikipediaPage.java
@@ -0,0 +1,7 @@
+package com.wikipathia.application.model;
+
+public class WikipediaPage {
+    public int pageid;
+    public Coordinate[] coordinates;
+    public Thumbnail thumbnail;
+}
