2e29d8dbe732773c21675ab595d64a4a873abe27
Juninger <marcus.juninger@gmail.com>
2022-01-10 16:59:59 +0100

    init commit

--- application/src/main/resources/static/stop_search.js
+++ application/src/main/resources/static/stop_search.js
@@ -35,6 +35,7 @@ function setArticle(article) {
 
 
 window.addEventListener("load", function () {
+    initMap();
     setArticle(article.parse);
     let showHide = document.getElementById("visibility");
     $(showHide).css("visibility", "hidden");
@@ -98,6 +99,29 @@ function loadPreviousStop(){
         alert("Du är redan på första hållplatsen!");
     }
 
+}
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: { lat: 0, lng: -180 },
+        mapTypeId: "terrain",
+    });
+    const flightPlanCoordinates = [
+        { lat: 37.772, lng: -122.214 },
+        { lat: 21.291, lng: -157.821 },
+        { lat: -18.142, lng: 178.431 },
+        { lat: -27.467, lng: 153.027 },
+    ];
+    const flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+    });
+
+    flightPath.setMap(map);
 }
 
 function loadStop(route) {
